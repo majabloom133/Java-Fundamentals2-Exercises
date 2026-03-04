9bb97d45804084675606e192e1a603837f5abf5a
Maja <maja.bloom@outlook.com>
2026-03-04 15:36:54 +0100

    Add exercise 1 - Length of a String, Add exercise 2 - Character at Index, Add exercise 3 - Index position of 'o'

---
+++ src/main/java/com/example/StringExercises.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class StringExercises {
+
+    public static void main(String[] args) {
+
+        // Exercise 1
+          // The length of the String "Java" is 4
+        String java = "Java";
+                System.out.println("Ex 1: Length is " + java.length());
+
+        // Exercise 2
+          // Char at index position 6 in "Long example sentence"? - is o
+        String sentence = "Long example sentence";
+        char letter = sentence.charAt(6);
+        System.out.println("Ex 2: Character at index 6 is: " + letter);
+
+        // Exercise 3
+        String longSentence = "Even longer example sentence";
+        // Find position using indexOf
+        int position = longSentence.indexOf("o");
+        System.out.println("Ex 3; Index position of 'o' is: " + position);
+
+        // Exercise 4
+
+        // Exercise 5
+
+        // Exercise 6
+
+    }
+}
