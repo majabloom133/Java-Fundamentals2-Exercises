ca6e8eb262f7c3a731c1c3aa30b7ff7c15ef00df
Maja <maja.bloom@outlook.com>
2026-03-04 09:06:20 +0100

    Add package, Import classes, Add class DateTimeExercises, Add Exercise 1 Print out Local Date.

---
+++ src/main/java/com/DateTimeExercises.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+public class DateTimeExercises {
+
+    public static void main(String[] args) {
+
+        // Exercise 1
+        LocalDate today = LocalDate.now();
+        System.out.println("Today is: " + today); // 2026-03-04
+
+        // Exercise 2
+
+        // Exercise 3
+
+        // Exercise 4
+
+    }
+}
